966b119ff865dd740643f9b18bc7a0ed623eaf97
Kolby ML <31669092+KolbyML@users.noreply.github.com>
2023-03-02 21:10:28 -0700

    Updated library name

--- lib/bitcoindart.dart
+++ lib/bitcoindart.dart
@@ -2,7 +2,7 @@
 // Distributed under the MIT software license, see the accompanying
 // file LICENSE or http://www.opensource.org/licenses/mit-license.php.
 
-library bitcoindart;
+library bitcoindart_light_client;
 
 import 'dart:async';
 import 'dart:convert';
